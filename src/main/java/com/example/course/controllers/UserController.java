692935e74451ee53836bd060118a1b79bd60c775
Jhonata02 <jhonata.pereira.andrade@ccc.ufcg.edu.br>
2025-06-27 11:29:41 -0300

    User insert

--- src/main/java/com/example/course/controllers/UserController.java
+++ src/main/java/com/example/course/controllers/UserController.java
@@ -4,11 +4,11 @@ import com.example.course.entities.User;
 import com.example.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,5 +30,15 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return user != null ? ResponseEntity.ok().body(user) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User newUser = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(newUser);
     }
 }
